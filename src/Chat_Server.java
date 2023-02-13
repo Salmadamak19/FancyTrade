958f13325873d0b3cae3ff467ba98a22c8360d3a
Oussema Ouakad <81573320+Godyrex@users.noreply.github.com>
2023-02-13 21:11:47 +0100

    Private chat
    from client<->server to clientA<->server<-->clientB

--- src/Chat_Server.java
+++ src/Chat_Server.java
@@ -8,77 +8,108 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-class ClientHandler implements Runnable {
-
-    private Socket client;
-    private DataInputStream dis;
-    private DataOutputStream dos;
-
-    ClientHandler(Socket client) throws IOException {
-        this.client = client;
-        this.dis = new DataInputStream(client.getInputStream());
-        this.dos = new DataOutputStream(client.getOutputStream());
-    }
-
-    @Override
-    public void run() {
-        while (true) {
-            try {
-                String message = dis.readUTF();
-                System.out.println("Message from client: " + message);
-                String[] receivedData = message.split(";;");
-                Connection connection;
-                connection = Database.getInstance().getCon();
-                String query = "SELECT prenom FROM user WHERE id_user = ?";
-                String query2 = "INSERT INTO message(from_user,to_user,message_text) VALUES(?,?,?)";
-                PreparedStatement statement2;
-                PreparedStatement statement;
-                try {
-                    statement = connection.prepareStatement(query);
-                    statement2 = connection.prepareStatement(query2);
-                    statement.setString(1, receivedData[0]);
-                    statement2.setString(1, receivedData[0]);
-                    statement2.setString(2, receivedData[1]);
-                    statement2.setString(3, receivedData[2]);
-                    statement2.executeUpdate();
-
-                    ResultSet resultSet = statement.executeQuery();
-                    String user_id = "not set";
-                    while (resultSet.next()) {
-                        user_id = resultSet.getString(1); //"zadaz";
-                    }
-
-                    String receiver_id = receivedData[1];
-                    String messageee = receivedData[2];
-                    dos.writeUTF(user_id + ";;" + receiver_id + ";;" + messageee);
-                } catch (SQLException ex) {
-                    Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
-                }
-
-            } catch (IOException e) {
-                System.out.println("Client disconnected");
-                break;
-            }
-        }
-    }
-}
-
 public class Chat_Server {
+
+    private static Map<String, Socket> clients = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(9999);
         System.out.println("Server started. Waiting for clients...");
-
         while (true) {
             Socket client = server.accept();
-            System.out.println("Client connected: " + client.getInetAddress().getHostAddress());
+            DataInputStream dis = new DataInputStream(client.getInputStream());
+            String clientID = dis.readUTF();
+            clients.put(clientID, client);
+            System.out.println("Client(" + clientID + ") connected: " + client.getInetAddress().getHostAddress());
 
-            ClientHandler clientHandler = new ClientHandler(client);
+            ClientHandler clientHandler = new ClientHandler(client, clientID);
             Thread thread = new Thread(clientHandler);
             thread.start();
+        }
+    }
+
+   static class ClientHandler implements Runnable {
+
+        private Socket client;
+        private String clientID;
+        private DataInputStream dis;
+        private DataOutputStream dos;
+
+        ClientHandler(Socket client, String clientID) throws IOException {
+            this.client = client;
+            this.clientID = clientID;
+            this.dis = new DataInputStream(client.getInputStream());
+            this.dos = new DataOutputStream(client.getOutputStream());
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String message = dis.readUTF();
+                    System.out.println("Message from client: " + message);
+                    String[] receivedData = message.split(";;");
+                    Connection connection;
+                    connection = Database.getInstance().getCon();
+                    String query = "SELECT prenom FROM user WHERE id_user = ?";
+                    String query2 = "INSERT INTO message(from_user,to_user,message_text) VALUES(?,?,?)";
+                    PreparedStatement statement2;
+                    PreparedStatement statement;
+                    try {
+                        statement = connection.prepareStatement(query);
+                        statement2 = connection.prepareStatement(query2);
+                        statement.setString(1, receivedData[0]);
+                        statement2.setString(1, receivedData[0]);
+                        statement2.setString(2, receivedData[1]);
+                        statement2.setString(3, receivedData[2]);
+                        statement2.executeUpdate();
+
+                        ResultSet resultSet = statement.executeQuery();
+                        String user_id = "not set";
+                        while (resultSet.next()) {
+                            user_id = resultSet.getString(1); //"zadaz";
+                        }
+
+                        String receiver_id = receivedData[1];
+                        String messageee = receivedData[2];
+                      //  dos.writeUTF(user_id + ";;" + receiver_id + ";;" + messageee);
+                        sendMessageToClient(receiver_id, message);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                } catch (IOException e) {
+                    System.out.println("Client disconnected");
+                    break;
+                }
+            }
+        }
+
+        public void sendMessage(String message) {
+            try {
+                dos.writeUTF(message);
+            } catch (IOException e) {
+                System.out.println("Error sending message to " + clientID + ": " + e.getMessage());
+                clients.remove(clientID);
+            }
+        }
+
+        public void sendMessageToClient(String clientID, String message) {
+            Socket socket = clients.get(clientID);
+            if (socket != null) {
+                try {
+                    new ClientHandler(socket, clientID).sendMessage(message);
+                } catch (IOException ex) {
+                    Logger.getLogger(Chat_Server.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                System.out.println("Error: no such client with id " + clientID);
+            }
         }
     }
 }
---
+++ src/User_Chat2.java
@@ -0,0 +1,128 @@
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static javafx.application.Application.launch;
+
+class ReceiverThread implements Runnable {
+
+    private DataInputStream dis;
+    private TextArea messages;
+
+    ReceiverThread(DataInputStream dis, TextArea messages) {
+        this.dis = dis;
+        this.messages = messages;
+    }
+        public String prenom(String id) {
+                    Connection connection;
+                    connection = Database.getInstance().getCon();
+                    String query = "SELECT prenom FROM user WHERE id_user = ?";
+                                        PreparedStatement statement;
+                                        String user_id = "not set";
+                                                            try {
+                        statement = connection.prepareStatement(query);
+                        statement.setString(1, id);
+
+                        ResultSet resultSet = statement.executeQuery();
+                        
+                        while (resultSet.next()) {
+                            user_id = resultSet.getString(1); //"zadaz";
+                        }
+       
+                    } catch (SQLException ex) {
+                        Logger.getLogger(Chat_Server.ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+        return user_id;
+        }
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                String message = dis.readUTF();
+                String[] receivedData = message.split(";;");
+
+                String senderID = receivedData[0];
+                String receiverID = receivedData[1];
+                String text = receivedData[2];
+                Platform.runLater(() -> messages.appendText(prenom(senderID) + " To " + prenom(receiverID) + " : " + text + "\n"));
+            } catch (IOException e) {
+                System.out.println("Server disconnected");
+                break;
+            }
+        }
+    }
+}
+
+public class User_Chat2 extends Application {
+    private DataInputStream dis;
+    private DataOutputStream dos;
+    private TextArea messages = new TextArea();
+    private TextField input = new TextField();
+    private Button send = new Button("Send");
+    private String clientID = "2";
+    private String receiverID = "1";
+
+    @Override
+    public void start(Stage Stage) {
+        VBox root = new VBox(10);
+        root.setPadding(new Insets(10));
+
+        messages.setPrefHeight(300);
+        messages.setEditable(false);
+
+        HBox inputArea = new HBox(10);
+        inputArea.getChildren().addAll(input, send);
+
+        root.getChildren().addAll(messages, inputArea);
+
+        send.setOnAction(event -> {
+            try {
+                dos.writeUTF(clientID + ";;" + receiverID + ";;" + input.getText());
+                messages.appendText("You: " + input.getText() + "\n");
+                input.clear();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        try {
+            Socket socket = new Socket("localhost", 9999);
+            dis = new DataInputStream(socket.getInputStream());
+            dos = new DataOutputStream(socket.getOutputStream());
+            dos.writeUTF(clientID);
+            ReceiverThread receiverThread = new ReceiverThread(dis, messages);
+            Thread thread = new Thread(receiverThread);
+            thread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(root, 400, 400);
+        Stage.setScene(scene);
+        Stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
