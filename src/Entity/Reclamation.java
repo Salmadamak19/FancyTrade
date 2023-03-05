94276b57f8a44a1fa4a48122b2517bdca31b55c1
fairouzkh <125240865+fairouzkh@users.noreply.github.com>
2023-03-05 22:35:29 +0100

    reclamaation ameliorer

--- src/Entity/Reclamation.java
+++ src/Entity/Reclamation.java
@@ -1,48 +1,43 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Entity;
 
-import java.sql.Timestamp;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import java.time.LocalDate;
 
 /**
  *
- * @author fairouzkhayat
+ * @author fairouz
  */
-
-   public class Reclamation {
-    private int id;
-    private String reclamation_Date;
-    private String reclamation_category_id;
-    private String user_id;
+public class Reclamation {
+     private int id;
+    private LocalDate reclamation_Date;
+    private int reclamation_category_id;
+   // private String user_id;
     private String reclamation_type;
-    private String description;
-    private String object;
+    private String contenu;
+    private String objet;
 
-    public Reclamation() {
+    public Reclamation(int id) {
+        this.id = id;
     }
 
-     
-        public Reclamation(String reclamation_category_id, String reclamation_type, String description) {
-        this.reclamation_category_id = reclamation_category_id;
-        this.reclamation_type = reclamation_type;
-        this.description = description;
-    }
-
-    public Reclamation(String reclamation_category_id, String reclamation_Date, String reclamation_type, String description) {
+    public Reclamation(LocalDate reclamation_Date, int reclamation_category_id, String reclamation_type, String contenu, String objet) {
         this.reclamation_Date = reclamation_Date;
-        this.reclamation_type = reclamation_type;
         this.reclamation_category_id = reclamation_category_id;
-        this.description = description;
+        this.reclamation_type = reclamation_type;
+        this.contenu = contenu;
+        this.objet = objet;
+    }
+
+    public Reclamation(int id, LocalDate reclamation_Date, int reclamation_category_id, String reclamation_type, String contenu, String objet) {
+        this.id = id;
+        this.reclamation_Date = reclamation_Date;
+        this.reclamation_category_id = reclamation_category_id;
+        this.reclamation_type = reclamation_type;
+        this.contenu = contenu;
+        this.objet = objet;
     }
 
     public int getId() {
@@ -53,20 +48,20 @@ import javafx.stage.Stage;
         this.id = id;
     }
 
-    public String getReclamation_category_id() {
+    public LocalDate getReclamation_Date() {
+        return reclamation_Date;
+    }
+
+    public void setReclamation_Date(LocalDate reclamation_Date) {
+        this.reclamation_Date = reclamation_Date;
+    }
+
+    public int getReclamation_category_id() {
         return reclamation_category_id;
     }
 
-    public void setReclamation_category_id(String reclamation_category_id) {
+    public void setReclamation_category_id(int reclamation_category_id) {
         this.reclamation_category_id = reclamation_category_id;
-    }
-
-    public String getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
     }
 
     public String getReclamation_type() {
@@ -77,43 +72,47 @@ import javafx.stage.Stage;
         this.reclamation_type = reclamation_type;
     }
 
-
-
-    public String getDescription() {
-        return description;
+    public String getContenu() {
+        return contenu;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setContenu(String contenu) {
+        this.contenu = contenu;
     }
 
-
-
-    public String getReclamation_Date() {
-        return reclamation_Date;
+    public String getObjet() {
+        return objet;
     }
 
-    /**
-     * @param reclamation_Date the reclamation_Date to set
-     */
-    public void setReclamation_Date(String reclamation_Date) {
-        this.reclamation_Date = reclamation_Date;
+    public void setObjet(String objet) {
+        this.objet = objet;
     }
 
-    /**
-     * @return the object
-     */
-    public String getObject() {
-        return object;
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + this.id;
+        return hash;
     }
 
-    /**
-     * @param object the object to set
-     */
-    public void setObject(String object) {
-        this.object = object;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Reclamation other = (Reclamation) obj;
+        return this.id == other.id;
     }
-}
 
+    @Override
+    public String toString() {
+        return "Reclamation{" + "id=" + id + ", reclamation_Date=" + reclamation_Date + ", reclamation_category_id=" + reclamation_category_id + ", reclamation_type=" + reclamation_type + ", contenu=" + contenu + ", objet=" + objet + '}';
+    }
     
-
+}
--- src/GUI/InterfaceReclamationController.java
+++ src/GUI/InterfaceReclamationController.java
@@ -1,69 +1,87 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package GUI;
 
 import Entity.Reclamation;
-import Services.Reclamation_services;
+import Services.ServiceReclamation;
 import java.io.IOException;
 import java.net.URL;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
-
-
-
-
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
  *
- * @author fairouzkhayat
+ * @author fairouz
  */
 public class InterfaceReclamationController implements Initializable {
 
     @FXML
-    private Button btnliste;
-    @Override
-    public void initialize(URL url, ResourceBundle rb) 
-    {}
-     @FXML
-     private void gotopage(ActionEvent event) throws IOException{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("list_reclamation.fxml"));
-    }
-    @FXML
-    private TextField idnum;
-    @FXML
-    private TextField iddate_reclamation;
-     @FXML
-    private TextArea idcontenu;
-    @FXML
     private TextField idobjet;
     @FXML
-    private ChoiceBox<?> idtype;
+    private Button btnlist;
     @FXML
     private Button idajouter;
+    @FXML
+    private TextField idcontenu;
+    @FXML
+    private DatePicker date;
+    @FXML
+    private TextField type;
+    @FXML
+    private TextField categorie;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void gotopage(ActionEvent event) throws IOException {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("listeReclamation.fxml"));
+        Parent root = loader.load();
+        Scene newScene = new Scene(root);
+        Stage stage = (Stage) btnlist.getScene().getWindow();
+        stage.setScene(newScene);
+        stage.show();
+    }
 
     @FXML
     private void ajouter(ActionEvent event) {
-        
-       Reclamation p = new Reclamation(idnum.getText(),iddate_reclamation.getText(),idcontenu.getText());
-       Reclamation_services s= new Reclamation_services();
-       s.ADD(p); 
+          ServiceReclamation srch = new ServiceReclamation();
+               
+            
+LocalDate daterec = date.getValue();
+int cat = Integer.parseInt(categorie.getText());
+ String typ = type.getText();
+  String contenu = idcontenu.getText();
+   String obj = idobjet.getText();  
+    
+     Reclamation res = new Reclamation( daterec ,cat, typ, contenu, obj);
+                      ServiceReclamation service = new ServiceReclamation();
+
+    service.ajouter(res);
+          JOptionPane.showMessageDialog(null, "Reclamation envoyé avec success !");
+          service.afficher();
     }
-  }  
+    }
     
 
